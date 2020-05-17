299b95e1b9f9ee6d63c516da46baaa0493f2a82b
zshlovezll <1396274109@qq.com>
2020-05-17 18:08:04 +0800

    host

--- dao/DButil.js
+++ dao/DButil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 function createConnection (){
     var connection = mysql.createConnection({
-        host:"127.0.0.1",
+        host:"101.132.136.167",
         port:"3306",
         user:'root',
         password:"bamboo",
